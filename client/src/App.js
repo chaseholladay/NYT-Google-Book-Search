470c2cd6330c6fa35664d11fd0d34dc4a0ffb76a
Chase Holladay <chaseholladay@gmail.com>
2018-12-05 19:34:48 -0600

    images

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,7 @@ const App = () => (
   <Router>
     <div>
       <Nav />
-      <img src="C:\Users\chase\Documents\codingBootcamp\homeworks\NYT-Google-Book-Search\client\src\TIN-332 Books from_large.jpg" alt="book banner"></img> 
-      <img src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj0-r_cgIrfAhUo9IMKHQNBCOoQjRx6BAgBEAU&url=https%3A%2F%2Fwww.thebanner.org%2Ftuned-in%2F2016%2F03%2Fbooks-from-the-banner-desk&psig=AOvVaw19As8xSLga-Ih-6EBotZfE&ust=1544144510124151" alt="book banner"></img>
+      <img src="./TIN-332 Books from_large.jpg" alt="book banner"/>
       <Route exact path="/" component={Search} />
       <Route exact path="/saved" component={Saved} />
       <Footer />
